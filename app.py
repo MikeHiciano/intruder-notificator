3cbadec1b556af6c4a656b7c1163f59cdaa5b9ad
Miguel Hiciano <mike29hiciano@gmail.com>
2021-03-10 19:15:38 -0400

    trying to run it

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def main():
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
-    updater.bot.setWebhook('https://intruder-notificator-test.herokuapp.com/bot' + TOKEN)
+    updater.bot.setWebhook('https://intruder-notificator-test.herokuapp.com/' + TOKEN)
     updater.idle()
 
 if __name__ == '__main__':
