148cd9e849c472ac230d90ad24e98a3ffdfc4c01
Miguel Hiciano <mike29hiciano@gmail.com>
2021-03-10 17:00:11 -0400

    testing heroku issue

---
+++ app.py
@@ -0,0 +1,47 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+
+TOKEN = os.environ.get('TOKEN')
+chat_id = int(os.environ.get('CHAT_ID'))
+PORT = int(os.environ.get('PORT', 5000))
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    """Start the bot."""
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://intruder-notificator-test.herokuapp.com/' + TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
